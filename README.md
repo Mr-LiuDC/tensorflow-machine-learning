5aa1ea2f1e1dd9e85ff0be47031abec844fad3b7
刘德财 <442823529@qq.com>
2021-03-24 11:42:12 +0800

    环境准备

---
+++ README.md
@@ -0,0 +1,9 @@
+# flask-machine-learning
+
+Flask 整合 Tensorflow 进行机器学习研究。
+
+
+### 安装依赖库
+```
+pip install -r requirements.txt -i https://pypi.doubanio.com/simple
+```
--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Hello Python !")
+import os
+
+from flask import Flask
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World !"
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
