695bacc870f77588fce0aeab8b37d92fd89d6d41
刘德财 <442823529@qq.com>
2021-04-01 13:47:07 +0800

    补充mnist预测接口

--- app/mnist/views.py
+++ app/mnist/views.py
@@ -1,6 +1,13 @@
-from flask import render_template
+import numpy as np
+import tensorflow as tf
+from flask import render_template, Flask, request, json
 
 from . import mnist
+from .training.convolution_training import CNN
+from .training.regression_training import RGN
+
+app = Flask(__name__)
+root_path = app.root_path
 
 
 @mnist.route('/')
@@ -10,4 +17,16 @@ def mnist_page():
 
 @mnist.route("/api/mnist", methods=['POST'])
 def mnist_mnist():
-    return ""
+    input_data = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
+
+    rgn_latest = tf.train.latest_checkpoint(root_path + '/ckpt/regression')
+    cnn_latest = tf.train.latest_checkpoint(root_path + '/ckpt/convolution')
+    rgn = RGN()
+    cnn = CNN()
+    # 恢复网络权重
+    rgn.model.load_weights(rgn_latest)
+    cnn.model.load_weights(cnn_latest)
+
+    pred_1 = rgn.model.predict(input_data.reshape(-1, 28, 28, 1)).flatten().tolist()
+    pred_2 = cnn.model.predict(input_data.reshape(-1, 28, 28, 1)).flatten().tolist()
+    return json.jsonify(results=[pred_1, pred_2])
