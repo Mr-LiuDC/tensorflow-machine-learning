4188bd0449f1aebfac691465642a574f86d75aac
LiuDecai <442823529@qq.com>
2021-04-01 21:13:28 +0800

    代码优化避免模型重新加载

--- app/mnist/views.py
+++ app/mnist/views.py
@@ -9,6 +9,14 @@ from .training.regression_training import RGN
 app = Flask(__name__)
 root_path = app.root_path
 
+rgn_latest = tf.train.latest_checkpoint(root_path + '/ckpt/regression')
+cnn_latest = tf.train.latest_checkpoint(root_path + '/ckpt/convolution')
+rgn = RGN()
+cnn = CNN()
+# 恢复网络权重
+rgn.model.load_weights(rgn_latest)
+cnn.model.load_weights(cnn_latest)
+
 
 @mnist.route('/')
 def mnist_page():
@@ -18,14 +26,6 @@ def mnist_page():
 @mnist.route("/api/mnist", methods=['POST'])
 def mnist_mnist():
     input_data = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
-
-    rgn_latest = tf.train.latest_checkpoint(root_path + '/ckpt/regression')
-    cnn_latest = tf.train.latest_checkpoint(root_path + '/ckpt/convolution')
-    rgn = RGN()
-    cnn = CNN()
-    # 恢复网络权重
-    rgn.model.load_weights(rgn_latest)
-    cnn.model.load_weights(cnn_latest)
 
     pred_1 = rgn.model.predict(input_data.reshape(-1, 28, 28, 1)).flatten().tolist()
     pred_2 = cnn.model.predict(input_data.reshape(-1, 28, 28, 1)).flatten().tolist()
